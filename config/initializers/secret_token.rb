025ffd1f3134b52eaca7713f20c56dc21281e0cf
cqin <cqin@cqin-laptop.(none)>
2011-03-01 13:50:06 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SimplestApp::Application.config.secret_token = 'cc6a6f1a0cf3d8eac6b58c6bb9921a37b20072f8109679f768791f27130862d70e2f156d168bce2adb3a6bc2c089a25f683217e7b0c359460aebd5ab702031b6'
